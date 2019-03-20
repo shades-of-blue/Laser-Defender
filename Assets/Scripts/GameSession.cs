edc6a9b8f68cc90108f805039dc4bd93c2e4cb95
saketh <shades-of-blue>
2019-03-20 21:59:25 +0530

    Initial Commit

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour {
+
+     int score = 0;
+
+    void Start()
+    {
+        int width = 450; // or something else
+        int height = 800; // or something else
+        bool isFullScreen = false;
+        Screen.SetResolution(width, height, isFullScreen);
+    }
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        int numberGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numberGameSessions > 1)
+        {
+            Destroy(gameObject);
+            score = 0;
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void AddToScore(int scoreValue)
+    {
+        score += scoreValue;
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+   
+
+ }
